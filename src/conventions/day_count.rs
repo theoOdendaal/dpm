1c3a2d21ada87641b05c97e9e3f7e6c9fe0e765f
theoOdendaal <theodendaal@gmail.com>
2024-09-14 22:37:44 +0200

    partial day count convention implementation

--- src/conventions/day_count.rs
+++ src/conventions/day_count.rs
@@ -1,5 +1,7 @@
 // Day count conventions.
 
+use chrono::NaiveDate;
+
 pub enum DayCountConventions {
     Thirty360Bond,
     ThirtyE360,
@@ -17,22 +19,48 @@ impl Default for DayCountConventions {
     }
 }
 
+impl DayCountConventions {
+    pub fn day_count(&self, d1: &NaiveDate, d2: &NaiveDate) -> f64 {
+        match self {
+            Self::Thirty360Bond => unimplemented!(),
+            Self::ThirtyE360 => unimplemented!(),
+            Self::ThirtyE360ISDA => unimplemented!(),
+            Self::ThirtyEPlus360 => unimplemented!(),
+            Self::Actual360 => Actual360::year_fraction(d1, d2),
+            Self::Actual365Fixed => Actual365Fixed::year_fraction(d1, d2),
+            Self::Actual365Actual => unimplemented!(),
+            Self::NonLeap365 => unimplemented!(),
+        }
+    }
+}
+
+//  --- Traits ---
+
 // Call using [NaiveDate].day_count(Actual365Fixed).
-pub trait DayCount {
-    fn day_count(&self, convention: DayCountConventions) -> Vec<f64>;
+pub trait YearFraction<A> {
+    fn year_fraction(d1: A, d2: A) -> f64;
 }
 
-/*
-trait DayCountFraction<A> {
-    fn year_fraction(first_date: &A, second_date: &A) -> f64;
-}
-
-struct Thirty360Bond;
-struct ThirtyE360;
-struct ThirtyE360ISDA;
-struct ThirtyEPlus360;
+//  --- Structs ---
+//struct Thirty360Bond;
+//struct ThirtyE360;
+//struct ThirtyE360ISDA;
+//struct ThirtyEPlus360;
 struct Actual360;
 struct Actual365Fixed;
-struct Actual365Actual;
-struct NonLeap365;
-*/
+//struct Actual365Actual;
+//struct NonLeap365;
+
+//  --- Trait implementations ---
+
+impl YearFraction<&NaiveDate> for Actual360 {
+    fn year_fraction(d1: &NaiveDate, d2: &NaiveDate) -> f64 {
+        (*d2 - *d1).num_days() as f64 / 360.0
+    }
+}
+
+impl YearFraction<&NaiveDate> for Actual365Fixed {
+    fn year_fraction(d1: &NaiveDate, d2: &NaiveDate) -> f64 {
+        (*d2 - *d1).num_days() as f64 / 365.0
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
-/*
 use std::str::FromStr;
 
 use chrono::{Months, NaiveDate};
 
 use dpm::conventions::business_day::{BusinessDay, BusinessDayConventions};
+use dpm::conventions::day_count::DayCountConventions;
 use dpm::core::sequence::Sequence;
 use dpm::iso::iso3166::CountryTwoCode;
-*/
+
 use dpm::resources::holiday_loader;
 
 // TODO Implement a logger for the holiday_loader module.
 fn main() {
-    // Holiday loader.
-
-    //let _res = holiday_loader::write_public_holidays(&Client::new(), 2022..=2024, "US");
-
-    let mut res = holiday_loader::HolidayLoader::new();
-    println!("{:?}", &res);
-
-    println!("{:?}", holiday_loader::read_saved_holidays_population());
-    let period = 2019;
-    let test = &mut res.load_if_not_available("ZA", period);
-    println!("{:?}", test);
-    println!("{:?}", res);
-    // Date range
-    /*
-    let country_code = CountryTwoCode::from_str("ZA");
-    let holidays = holiday_loader::load_country_calendar(&country_code.unwrap().to_string())
-        .unwrap_or_default();
-
     let start = NaiveDate::from_ymd_opt(2023, 12, 31).unwrap();
     let end = NaiveDate::from_ymd_opt(2024, 12, 31).unwrap();
     let step = Months::new(2);
     let bdc = BusinessDayConventions::ModifiedFollowing;
+    let dcc = DayCountConventions::Actual360;
+
+    let country_code = CountryTwoCode::from_str("ZA").unwrap();
+    /*
+        holiday_loader::write_public_holidays(
+            &Client::new(),
+            start.year() as u32..=end.year() as u32,
+            &country_code.to_string(),
+        )
+        .unwrap();
+    */
+
+    let holidays = holiday_loader::read_country_calendar(&country_code.to_string()).unwrap();
 
     let seq_res = NaiveDate::seq(start, end, step).business_day(&bdc, &holidays);
 
     println!("{:?}", seq_res);
-    */
+
+    let seq_frac: Vec<f64> = seq_res
+        .iter()
+        .skip(1)
+        .zip(seq_res.iter())
+        .map(|(a, b)| dcc.day_count(b, a))
+        .collect();
+
+    println!("{:?}", seq_frac);
 }
